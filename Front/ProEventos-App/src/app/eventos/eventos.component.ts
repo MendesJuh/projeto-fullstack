707ff06cbb3c955b6cacca1739e0a1773a0d3c96
Juliana Mendes <juliana-dantas@live.com>
2023-08-08 17:29:43 -0300

    Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,17 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos:any;
+  public eventos:any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 150;
+  marginImg:number =2;
+  showImg = true;
+  private _filterList: string = "";
+
+  public get filterList (): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string){
+    this._filterList = value;
+    this.eventosFiltrados = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+  }
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (      evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
+  alterarImg(){
+    this.showImg = !this.showImg;
+  }
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => {this.eventos = response},
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       (error) => {console.log(error)}
     );
 
